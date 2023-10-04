acd65f6b5c3ebd06ef960eb8620aba8912f954f5
LudaTverday <Ludachka22@gmail.com>
2023-10-04 14:47:01 +0300

    initial code

--- src/telran/multithresding/ExpData.java
+++
@@ -1,5 +0,0 @@
-package telran.multithresding;
-
-public record ExpData(int updateProb, int nOperations) {
-
-}
--- src/telran/threads_race/RangeSleep.java
+++
@@ -1,5 +0,0 @@
-package telran.threads_race;
-
-public record RangeSleep(int minValue, int maxValue) {
-
-}
